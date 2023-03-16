b176f3c2b0ef5b9b2ac5ea7e76dcc157344b5ed9
Newaz579 <newazsharif329@gmail.com>
2023-03-16 15:41:57 +0600

    git quiz

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -16,7 +16,7 @@ const Blog = () => {
                 <strong>
                     Here are a few benefits of Context API that give it an edge:
                     The Context API helps share data between components which you can’t easily share with props, for example, complex data objects.
-                    React Context API provides a way to send data like userid, auth, and theme data through the component tree without sending any props manually at every level.
+                    React Context API provides a way to send data like userId, auth, and theme data through the component tree without sending any props manually at every level.
                     We can also share specific states with multiple components instead through props manually. In some use cases, ideal for theming, localization, authentication etc.
                 </strong>
             </p>
